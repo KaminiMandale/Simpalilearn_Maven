514a2a0187d1e0ba63084137a8cd69c304c3c2e4
Your Name <you@example.com>
2023-04-30 09:36:10 +0530

    save

---
+++ src/test/java/testcases/HomeTestpage.java
@@ -0,0 +1,11 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class HomeTestpage extends BaseClass {
+	@Test
+	public void test3() {
+		System.out.println(" Inside test 3");
+	}
+
+}
